6811f6d2a75a307a388918c1b1bf71533b15269a
sachin <sachin.sriva8@gmail.com>
2024-01-05 01:24:03 +0530

    Updated ViteConfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://sachinsriva8.github.io/Space/",
+  base: "/Space",
   plugins: [react()],
 });
